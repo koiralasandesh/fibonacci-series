8ec3ae2af752ba7707fa5d3fd606bb8b8beec5fc
Sandesh <xeroxysandesh@gmail.com>
2019-06-17 02:50:43 -0500

    code cleanup and formatting

--- src/main.c
+++ src/main.c
@@ -3,22 +3,32 @@
 
 int main()
 {
-    int number, i, j,k,l,sum=1;
-    printf("Enter the number of terms:\n");
-    scanf("%d", &number);
-    j=0;
-    k=1;
-    printf("First %d terms of Fibonacci numbers are: \n",number);
-    printf("%d\n%d\n",j,k);
-    for (i=2;i<number;i++){
-       l=j+k;
-       sum+=l;
-       printf("%d\n",l);
-       j=k;
-       k=l;
+    
+    printf("===========================================\n");
+    while(true){
+        int number, i, j,k,l,sum=1;
+        printf("Enter the number of terms, 0 to exit:\n");
+        scanf("%d", &number);
+        if(number==0){
+            break;
+        }
+        j=0;
+        k=1;
+        printf("-------------------------------------------\n");
+        printf("First %d terms of Fibonacci numbers are: \n",number);
+        printf("%d\n%d\n",j,k);
+        for (i=2;i<number;i++){
+        l=j+k;
+        sum=sum+l;
+        printf("%d\n",l);
+        j=k;
+        k=l;
+        }
 
-
+        printf("-------------------------------------------\n");
+        printf("The sum value of the above sequence is:\n%d\n",sum);
+        printf("===========================================\n");
     }
-    printf("The sum value of the above sequence is:\n%d",sum);
     return 0;
+
 }
